14f5569ec5a28116a031a7dae8ed85a5bf458924
Surya Prakash <surya512002@gmail.com>
2026-01-25 16:27:17 +0000

    Added SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// 1. Import the SpeedInsights component
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* 2. Add the component here, inside the body */}
+        <SpeedInsights />
       </body>
     </html>
   );
